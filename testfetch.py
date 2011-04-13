96f2b78081071595bf2b37379830c1d6577c4b3d
imcaptor <imcaptor@gmail.com>
2011-04-13 16:20:26 +0800

    删除中文测试代码

--- testfetch.py
+++ testfetch.py
@@ -5,5 +5,4 @@ from BeautifulSoup import BeautifulSoup
 
 page = urllib2.urlopen('http://www.okooo.com/Lottery06/SoccerIndex.php?LotteryType=ToTo&LotteryNo=11037')
 
-print '测试'
 print page.read().decode('GBK').encode('UTF-8')
