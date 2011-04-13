8446e8fe0e26679574cee724ef61a97d30b47a31
imcaptor <imcaptor@gmail.com>
2011-04-13 18:00:49 +0800

    增加parser代码，但是内容是异步加载的，需要分析js，实现浏览器功能。

--- testfetch.py
+++ testfetch.py
@@ -5,4 +5,23 @@ from BeautifulSoup import BeautifulSoup
 
 page = urllib2.urlopen('http://www.okooo.com/Lottery06/SoccerIndex.php?LotteryType=ToTo&LotteryNo=11037')
 
-print page.read().decode('GBK').encode('UTF-8')
+parser = BeautifulSoup(page)
+
+#print parser.html.head.title
+
+#print page.read().decode('GBK').encode('UTF-8')
+
+#以下代码证明BeautifulSoup可以自动转码
+#print parser.html.head.title
+#matchList = parser.find('div', align='center')
+
+print parser.html
+'''
+i = 1
+for match in matchList:
+    i = i + 1
+    print i
+    print match
+'''
+#print matchList
+print 'finished'
