0334c484b9d6ac405283b472f3c1f02950367b0b
imcaptor <imcaptor@gmail.com>
2011-04-14 12:55:54 +0800

    使用lxml实现代码。

--- testfetch.py
+++ testfetch.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import urllib2
-from BeautifulSoup import BeautifulSoup
+from lxml import html
 
-page = urllib2.urlopen('http://www.okooo.com/Lottery06/SoccerIndex.php?LotteryType=ToTo&LotteryNo=11037')
-
-parser = BeautifulSoup(page)
-
-#print parser.html.head.title
-
-#print page.read().decode('GBK').encode('UTF-8')
-
-#以下代码证明BeautifulSoup可以自动转码
-#print parser.html.head.title
-#matchList = parser.find('div', align='center')
-
-print parser.html
-'''
-i = 1
-for match in matchList:
-    i = i + 1
-    print i
-    print match
-'''
-#print matchList
+html = html.parse('http://www.okooo.com/Lottery06/SoccerIndex.php?LotteryType=ToTo&LotteryNo=11038')
+link = html.getroot().xpath('//table[@id="TableBorder"]')
+footballs = link[0].xpath('//tr[@class="WhiteBg BlueWord" or @class="ContentLight BlueWord"]')
+items = footballs[0].findall("td")
+print items
 print 'finished'
